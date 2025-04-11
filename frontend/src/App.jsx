5775bc23d3d671a85cb71783681aa21b1992b0fc
XuanGiaHanNguyen <han316@usf.edu>
2025-04-11 17:38:36 -0400

    updated frontend layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 
 import Landing from './pages/Landing'
-import Searching from './pages/Search'
 import Dock from './pages/Dock'
-import Detail from './pages/Detail'
 import Auth from './pages/Auth'
 
 function App() {
@@ -14,9 +12,7 @@ function App() {
    <BrowserRouter>
     <Routes>
       <Route path='/' element={<Landing/>}/>
-      <Route path='/search' element={<Searching/>}/>
       <Route path='/dashboard/:userId' element={<Dock/>}/>
-      <Route path='/detail/:productId' element={<Detail/>}/>
       <Route path='/auth' element={<Auth/>}/>
     </Routes>
    </BrowserRouter>
